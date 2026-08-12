2378765aff4b23bb6b17395c0879e8552676a7d2
JOSHUAJEBARAJ <joshuajebaraj.z@gmail.com>
2026-08-13 04:16:43 +0530

    add event emitter

--- pyagent/loop.py
+++ pyagent/loop.py
@@ -3,7 +3,11 @@ import litellm
 import tools
 import json 
 MAX_ITERATIONS=10
-def run(user_message : str,messages: list= None)-> list:
+def run(user_message : str,messages: list= None,emit=None)-> list:
+    # dummy emit
+    if emit is None:
+        emit= lambda event:None
+    emit({"type":"agent_start"})
     if messages is None:
         messages=[
             {
@@ -24,6 +28,7 @@ def run(user_message : str,messages: list= None)-> list:
     current_iteration=0
 
     while current_iteration<MAX_ITERATIONS:
+        emit({"type": "turn_start"})
         response=litellm.completion(
             model="gemini/gemini-flash-latest",
             messages=messages,
@@ -31,19 +36,24 @@ def run(user_message : str,messages: list= None)-> list:
         )
         response_message= response.choices[0].message
         messages.append(response_message.model_dump())
+        emit({"type": "assistant_message", "message": response_message.model_dump()})
         tools_calls= response_message.tool_calls
         if not tools_calls:
-            print(response_message.content)
+            # print(response_message.content)
             break
         for tool_call in tools_calls:
             tool_name=tool_call.function.name
             fn=tools.TOOL_FUNCTIONS[tool_name]
             tool_args=json.loads(tool_call.function.arguments)
+            is_error=False 
+            emit({"type": "tool_execution_start", "tool_call_id": tool_call.id, "tool_name": tool_name, "args": tool_args})
             try:
                 resp=fn(**tool_args)
             except Exception as e:
                 resp=str(e)
+                is_error=True
+            emit({"type": "tool_execution_end", "tool_call_id": tool_call.id, "tool_name": tool_name, "result":resp, "is_error": is_error})
             messages.append({"role":"tool","tool_call_id": tool_call.id,"content":resp})
         current_iteration+=1
-
+    emit({"type": "agent_end", "messages": messages})
     return messages
--- pyagent/main.py
+++ pyagent/main.py
@@ -1,8 +1,28 @@
 import loop 
 
 
+# print the events
+
+def print_event(event):
+    kind=event["type"]
+    if kind == "tool_execution_start":
+        print(f"[tool] {event['tool_name']}({event['args']})")
+
+    elif kind == "tool_execution_end":
+        status = "ERROR" if event["is_error"] else "ok"
+        result = str(event["result"])
+        if len(result) > 300:
+            result = result[:300] + "...(truncated)"
+        print(f"[tool result: {status}] {result}")
+
+    elif kind == "assistant_message":
+        content = event["message"].get("content")
+        if content:
+            print(content)
 
 message=None
 while True:
-    prompt=input(">")
-    message=loop.run(prompt,message)
+    prompt=input("> ")
+    message=loop.run(prompt,message,emit=print_event)
+
+
